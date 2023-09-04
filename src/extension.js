725b73008db9999bab9aa12f15fe239d26eddd8e
lokismelc <elviscu90@gmail.com>
2023-09-04 18:03:30 +0100

    all works with core.js

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,12 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
+console.log(name)
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
+console.log(isbn10)
 
 // Do not modify this basket object directly
 const basket = {
@@ -41,11 +43,20 @@ const basket = {
   voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
 
+const basket
+      name: Orange
+      quantity: 4
+      price: 0.75
+
+let price = 1 + 1
+
+const 
+
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = [0]
 
 // Do not edit this exported object
 module.exports = {
